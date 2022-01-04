c11da012397364818d6d0704cc73a53dbbdbbd16
Batuhan <91565731+BizonTR@users.noreply.github.com>
2022-01-04 17:07:15 +0300

    Add files via upload

---
+++ LeibnizFormula.py
@@ -0,0 +1,21 @@
+a=1
+check=1 #(1 for positives, 0 for negatives)
+negatives=[]
+positives=[]
+for i in range (100): # (getting closer to pi with each steps.)
+    num=1/a
+    if check==1:
+        check=0
+        positives.append(num)
+        a+=2
+    else:
+        check=1
+        negatives.append(num)
+        a+=2
+total_positives=0
+for i in positives:    
+    total_positives+=i
+total_negatives=0
+for i in negatives:
+    total_negatives+=i
+print(4*(total_positives-total_negatives))
